36cb53f27d37ef1ec3c3fafbf776ee1fa1a3a690
Cartago <cartago.relaxingcup@gmail.com>
2021-10-28 03:39:34 +0200

    ERC20.sol: Añadimos SafeMatch al token, y los eventos Transfer() - Approval()

--- 01_Blockchains/custom_token/ERC20.sol
+++ 01_Blockchains/custom_token/ERC20.sol
@@ -3,8 +3,11 @@ pragma solidity >=0.4.4 <=0.8.9;
 pragma experimental ABIEncoderV2;
 import "./SafeMath.sol";
 
-//Interface de nuestro token ERC20
+//Interface de nuestro token ERC20-----------------------------------
 interface IERC20{
+    
+    //INTERFACE BASICAS-----------------------------------
+    
     //Devuelve la cantidad de tokens en existencia
     function totalSupply() external view returns (uint256);
 
@@ -23,9 +26,22 @@ interface IERC20{
     //Devuelve un valor booleano con el resultado de la operacion de paso de una cantidad de tokens usando el metodo allowance()
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
     
+    //EVENTOS-----------------------------------
+    
+    //Evento que se de debe emitir cuando una cantidad de tokens pase de un origen a un destino
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    //Evento que se debe emitir cuando se establece una asignacion con el metodo allowance()
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    
 }
 
 contract ERC20Basic is IERC20{
+    
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+    event Approval(address indexed owner, address indexed spender, uint256 tokens);
+    
+    using SafeMath for uint256;
     
     function totalSupply() public override view returns (uint256){
         return 0;
