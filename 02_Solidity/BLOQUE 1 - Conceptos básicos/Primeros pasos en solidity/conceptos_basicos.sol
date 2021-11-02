c30f54443059d119b05e3322f3b1f1cbc8c7de5f
Cartago <cartago.relaxingcup@gmail.com>
2021-11-02 03:51:00 +0100

    02 - 05 - C21 - conceptos_basicos.sol : añadimos comentarios de prueba al contrato

--- 02_Solidity/BLOQUE 1 - Conceptos básicos/Primeros pasos en solidity/conceptos_basicos.sol
+++ 02_Solidity/BLOQUE 1 - Conceptos básicos/Primeros pasos en solidity/conceptos_basicos.sol
@@ -1,11 +1,20 @@
+// Indicamos la version
 pragma solidity >=0.4.0 <0.8.9;
+// Importamos el archivo ERC20.sol que esta en el directorio de trabajo
 import "./ERC20.sol";
 
+
+/// @notice Lo que hace el contrato o su funcionalidad (en formato natspec, recomendado para publicar contratos)
 contract PrimerContrato{
     
+    //En esta variable se encuentra la direccion de la persona que despliega elcontrato
     address owner;
     ERC20Basic token;
 
+    /*
+    Guardamo en la variable owner la direccion de la persona que despliega el contrato y ademas
+    inicializamos el numero de tokens
+    */
     constructor() public {
         owner=msg.sender;
         token = new ERC20Basic(1000);
