3497eb0e3dd3b16e5181cfd13c30273254e5b438
Cartago <cartago.relaxingcup@gmail.com>
2021-11-02 03:43:53 +0100

    02_Solidity - Seccion 5 - Añadimos ERC.sol y SafeMath.sol al proyecto y le añadimos el constructor

--- 02_Solidity/BLOQUE 1 - Conceptos básicos/Primeros pasos en solidity/conceptos_basicos.sol
+++ 02_Solidity/BLOQUE 1 - Conceptos básicos/Primeros pasos en solidity/conceptos_basicos.sol
@@ -1,5 +1,13 @@
 pragma solidity >=0.4.0 <0.8.9;
+import "./ERC20.sol";
 
 contract PrimerContrato{
     
+    address owner;
+    ERC20Basic token;
+
+    constructor() public {
+        owner=msg.sender;
+        token = new ERC20Basic(1000);
+    }
 }
