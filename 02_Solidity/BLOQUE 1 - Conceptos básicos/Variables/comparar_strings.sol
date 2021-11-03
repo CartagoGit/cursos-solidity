2dcb6ece70dea30c2024f75103bf2051b0c5b4e5
Cartago <cartago.relaxingcup@gmail.com>
2021-11-03 01:16:38 +0100

    02-05-c39/ comparar_strings.sol: Creamos el archivo con las funciones para comparar dos Strings en solidity)

---
+++ 02_Solidity/BLOQUE 1 - Conceptos básicos/Variables/comparar_strings.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.4.4 <0.8.9;
+
+contract comparar{
+    
+    // Con estas funciones podriamos comparar strings haciendolo más eficiente para gastar menos gas.
+    function compareStrings(string memory a, string memory b) public pure returns (bool) {    
+        return compareMemory(bytes(a), bytes(b));
+    }
+    
+    function compareMemory(bytes memory a, bytes memory b) internal pure returns (bool) {
+        return (a.length == b.length) && (keccak256(a) == keccak256(b));
+    }
+    
+}
