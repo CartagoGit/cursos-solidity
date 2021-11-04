e7806bac17004aedb8f61e4fa5aa0bd3ed72074e
Cartago <cartago.relaxingcup@gmail.com>
2021-11-05 00:41:50 +0100

    02-17-c85/ notas.sol: Declaraciones inciales.

--- 02_Solidity/BLOQUE 2 - Proyectos Reales/Sistema universitario/notas.sol
+++ 02_Solidity/BLOQUE 2 - Proyectos Reales/Sistema universitario/notas.sol
@@ -1,4 +1,26 @@
-// SPDX-Licence-Identifier: MIT
-pragma solidity >=0.4.4 <0.8.9;
-pragma experimental ABIEncoderV2;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.8.9;
+//pragma experimental ABIEncoderV2;
 
+contract contractNotas{
+    
+    // Direccion del profesor
+    address public profesor;
+    
+    // Constructor
+    constructor(){
+        profesor = msg.sender;
+    }
+    
+    // Mapping para relacionar el hash de la indentidad del alumno
+    mapping (bytes32 => uint) mapNotas;
+    
+    // Array de alumnos que pidan revision de examenes
+    string [] revisiones;
+    
+    // Eventos
+    event event_alumno_evaluado(bytes32);
+    event event_revision(string);
+    
+    
+}
