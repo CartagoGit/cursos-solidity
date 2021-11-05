7289eddf4a04e53fd6fe29eabdd3cacc6617c42e
Cartago <cartago.relaxingcup@gmail.com>
2021-11-05 03:03:14 +0100

    02-18-c92/ votaciones.sol: Añadimos funcion para presentar la candidatura a las votaciones

--- 02_Solidity/BLOQUE 2 - Proyectos Reales/02_Votaciones/votaciones.sol
+++ 02_Solidity/BLOQUE 2 - Proyectos Reales/02_Votaciones/votaciones.sol
@@ -21,4 +21,20 @@ contract votacion{
     
     // Lista de votantes en hash para que no se sepa quien ha votado y sea unico
     bytes32 [] votantes;
+    
+    //Funcion para presentar candidatura a las votaciones
+    function presentarCandidatura(string memory _nombre, uint _edad, string memory _idPersona) public{
+        
+        //Comprobar si el candidato es mayor de edad
+        require(_edad>18, "El candidato tiene que ser mayor de edad");
+        
+        //Hash de los datos del candidatos
+        bytes32 hash_candidato = keccak256(abi.encodePacked(_nombre, _edad, _idPersona));
+        
+        //Almacenar el hash de los datos del candidato ligado a su nombre;
+        mapIdCandidato[_nombre]=hash_candidato;
+        
+        //Añadimos al candidato en la lista
+        candidatos.push(_nombre);
+    }
 }
