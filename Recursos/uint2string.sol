5b2e310daa2c1686589c8f1fe24ae937df01850e
Cartago <cartago.relaxingcup@gmail.com>
2021-11-05 04:08:24 +0100

    Añadimos el recurso util uint2string.sol con 2 funciones dependiendo del pragma a usar

---
+++ Recursos/uint2string.sol
@@ -0,0 +1,45 @@
+//Funcion auxiliar que transforma un uint a un string
+
+// Funcion para antes de 0.8.0
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = byte(uint8(48 + _i % 10));
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+
+    // Funcion uint2str adaptada para posterior a 0.8.0
+
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
