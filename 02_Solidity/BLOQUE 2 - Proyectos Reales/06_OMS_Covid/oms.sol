82acdb15e3823672374d13caee401bb4fbdbfb0c
Cartago <cartago.relaxingcup@gmail.com>
2021-11-06 06:57:01 +0100

    Fix desde vsCode para lintear correctamente el codigo

--- 02_Solidity/BLOQUE 2 - Proyectos Reales/06_OMS_Covid/oms.sol
+++ 02_Solidity/BLOQUE 2 - Proyectos Reales/06_OMS_Covid/oms.sol
@@ -1,33 +1,32 @@
 //SPDX-License-Identifier: GPL.3.0
 pragma solidity >=0.8.0 <=0.9.0;
-
 contract OMS_Covid{
 
     //Direccion de la OMS
     address public owner;
-    
-    constructor(){
-        owner=msg.sender;
+
+    constructor () {
+        owner = msg.sender;
     }
 
     // Mapping para relacionar los centros de salud (direccion/address) con la validez del sistema de gestion
     mapping (address=> bool) map_validated_centers;
-    
+
     //Array de direcciones que almacene los contratos de salud validados
-    address [] public contract_centers;
+    address[] public contract_centers;
 
     //Eventos
     event event_NuevoCentroValidado(address);
     event event_NuevoContraro(address, address);
 
     //Modificador que permita la ejeciucion unicamente a la ONS
-    modifier onlyONS(){
-        require(msg.sender == owner);
+    modifier onlyOMS(){
+        require(msg.sender == owner, "Solo puede ejecutarlo la OMS");
         _;
     }
 
     //Funcion para validar nuevos centros de salud que pueda autogestionarse
-    function validateCenter(address _center) public onlyONS(){
+    function validateCenter(address _center) public onlyOMS(){
         map_validated_centers[_center] = true;
     }
 
