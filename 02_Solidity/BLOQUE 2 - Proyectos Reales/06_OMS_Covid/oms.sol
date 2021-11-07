cbce136a6cbec79bc783f6016598c57eb34f4e5b
Cartago <cartago.relaxingcup@gmail.com>
2021-11-07 05:29:21 +0100

    02-22-c154/ oms.sol: map de centro salud con su contrato. Modificador para autogestion del contrato por el centro

--- 02_Solidity/BLOQUE 2 - Proyectos Reales/06_OMS_Covid/oms.sol
+++ 02_Solidity/BLOQUE 2 - Proyectos Reales/06_OMS_Covid/oms.sol
@@ -1,17 +1,20 @@
-//SPDX-License-Identifier: GPL.3.0
-pragma solidity >=0.8.0 <=0.9.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <=0.9.0;
 
 contract OMS_Covid {
     //Direccion de la OMS
     address public owner;
+
 
     constructor() {
         owner = msg.sender;
     }
 
     // Mapping para relacionar los centros de salud (direccion/address) con la validez del sistema de gestion
-    mapping(address => bool) map_validatedCenters;
-
+    mapping(address => bool) public map_validatedCenters;
+    //Relacionar una direccion de un Centro de Salud con su contrato
+    mapping (address=>address) public map_ContractCenter;
+    
     //Array de direcciones que almacene los contratos de salud validados
     address[] public arr_contractCenters;
     //Array de solicitudes de los centros para crear un contrato para el centros
@@ -27,7 +30,8 @@ contract OMS_Covid {
         require(msg.sender == owner, "Solo puede ejecutarlo la OMS");
         _;
     }
-
+    
+    
     //Funcion para que el centro solicite un contrato
     function requestContract() public {
         arr_requestContract.push(msg.sender);
@@ -48,19 +52,19 @@ contract OMS_Covid {
 
     //Funcion que permita crear un contrato inteligente
     function createContracts() public {
-        require(
-            map_validatedCenters[msg.sender],
-            "El centro de salud no tiene permiso para crear el contrato"
-        );
+        require(map_validatedCenters[msg.sender],"El centro de salud no tiene permiso para crear el contrato");
         address addressCenterContract = address(new centerContract(msg.sender));
         //Almacenamos la direccion del nuevo contrato en el array
         arr_contractCenters.push(addressCenterContract);
+        // Relacion entre el centro de salud y su contrato;
+        map_ContractCenter[msg.sender] = addressCenterContract;
         //evento
         emit event_newContract(msg.sender, addressCenterContract);
     }
 }
 
 // -----------------------------------------------------------------------
+// Contrato autogestiobale por el centro de Salud
 contract centerContract {
     address addressContract;
     address addressCenter;
@@ -68,5 +72,10 @@ contract centerContract {
     constructor(address _center) {
         addressContract = address(this);
         addressCenter = _center;
+    }
+    
+    modifier onlyThisCenter(){
+        require(msg.sender==addressCenter, "Solo puede ser controlado por el centro de salud correspondiente");
+        _;
     }
 }
