4befb8c6e192e5032fbe48001d4a82bf1a92efad
Cartago <cartago.relaxingcup@gmail.com>
2021-11-07 06:47:11 +0100

    02-23-c163/ BLOQUE 3: Añadimos los archivos, librerias y recursos basicos para empezar el proyecto de una aseguradora

---
+++ 02_Solidity/BLOQUE 3 - Proyecto final/InsuranceFactory.sol
@@ -0,0 +1,8 @@
+pragma solidity >= 0.8.0 <0.8.9;
+import "./OperacionesBasicas.sol";
+import "./ERC20.Sol";
+
+//Contrato para la compañia de seguros
+contract InsuranceFactory is OperacionesBasicas{
+    
+}
