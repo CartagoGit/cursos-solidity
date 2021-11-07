5c4117dfbd7d38ae146308f09135fec494be2a68
Cartago <cartago.relaxingcup@gmail.com>
2021-11-07 06:54:17 +0100

    02-23-c164/ InsuranceFactory.sol: Añadimos las direcciones del contrato, de la aseguradora y un array con la lista de clientes

--- 02_Solidity/BLOQUE 3 - Proyecto final/InsuranceFactory.sol
+++ 02_Solidity/BLOQUE 3 - Proyecto final/InsuranceFactory.sol
@@ -1,8 +1,23 @@
+// SPDX-License-identifier: MIT
 pragma solidity >= 0.8.0 <0.8.9;
 import "./OperacionesBasicas.sol";
 import "./ERC20.Sol";
 
 //Contrato para la compañia de seguros
 contract InsuranceFactory is OperacionesBasicas{
+    constructor () {
+        token = new ERC20Basic(100);
+        Insurance = address(this);
+        Aseguradora = payable(msg.sender);
+    }
+    
+    //Instancia del contrato token
+    ERC20Basic private token;
+    
+    //Declaracion de las direcciones
+    address Insurance;
+    address payable public Aseguradora;
+    
+    address[] DireccionesAsegurados;
     
 }
