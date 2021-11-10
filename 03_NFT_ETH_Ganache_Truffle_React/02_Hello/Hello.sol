5846fc0c7186e67a2e6e7c021f8a71882dd68426
Cartago <cartago.relaxingcup@gmail.com>
2021-11-10 03:07:25 +0100

    03-08-c36: Creamos el archivo Hello.sol con el que trabajaremos con truffle

---
+++ 03_NFT_ETH_Ganache_Truffle_React/02_Hello/Hello.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIN 
+pragma solidity >=0.8.0 <=0.8.10;
+
+contract Hello {
+    string public message = "Hola Mundo";
+    
+    function getMessage() public view returns (string memory){
+        return message;
+    }
+    
+    function setMessage(string memory _message) public{
+        message = _message;
+    }
+}
